7f6cda0efd0441cd05f6aaa21fa432bf568d59a2
RocketScience98 <luisalbertodavila@gmail.com>
2021-05-06 18:31:30 -0500

    donerino

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -12,12 +13,13 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+   /* return Recipe.deleteMany()*/
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -25,3 +27,56 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  //Iteracion 2?
+  //Models comentar una vez agregado
+/*Recipe.create(data, (error, newCat) => {
+     if(error){
+         console.log(error)
+     }
+     console.log(newCat)
+ })*/
+ 
+ //Iteracion 3.1
+ /*
+ Recipe.insertMany(data).then((value) => {
+  console.log(value);
+  mongoose.connection.close()
+}).catch(err => {
+  console.log(err);
+
+ 
+});
+Iteracion 3.2
+     Recipe.insertMany(data)
+      .then((data) => {
+        data.forEach((data) => {
+          console.log(`Recipe name is: ${data.title}`);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+*/
+      //Iteration 4
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { $set: { duration: 100 } },
+        { new: true }
+      )
+        .then((recipe) => console.log("Success!"))
+        .catch((error) => console.error(error));
+
+    //Iteration 5
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then((recipe) => console.log("Success"))
+      .catch((error) => console.error(error));
+//Iteration 6
+mongoose.connection
+.close()
+.then(() => {
+  console.log(`Disonnected from the database`);
+})
+.catch((error) => {
+  console.error("Error disconnecting from the database");
+});
